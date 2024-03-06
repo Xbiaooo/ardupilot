cf4cad12de483e2126f267218cbba20fb4a1f858
Xbiaooo <2463381729@qq.com>
2024-03-06 16:09:08 +0800

    Update mode_DrawStar.cpp

--- ArduCopter/mode_DrawStar.cpp
+++ ArduCopter/mode_DrawStar.cpp
@@ -10,7 +10,7 @@
 
 bool ModeDrawStar::init(bool ignore_checks)
 {
-    path_num = 0;   //航点号清0，从别的模式切到此模式时，可以飞出一个新的五角星
+    path_num = 1;   //航点号清0，从别的模式切到此模式时，可以飞出一个新的五角星
     generate_path();    //生成五角星航线
     pos_control_start();    //开始位置控制
 
@@ -22,23 +22,15 @@ void ModeDrawStar::generate_path()
 {
     float radius_cm = g2.star_radius_cm;
 
-    wp_nav->get_wp_stopping_point(path[0]);
+    wp_nav->get_wp_stopping_point(path[1]);
 
     //xy轴绘制五角星
-    // path[1] = path[0] + Vector3f(1.0f, 0, 0) * radius_cm;
-    // path[2] = path[0] + Vector3f(-cosf(radians(36.0f)), -sinf(radians(36.0f)), 0) * radius_cm;
-    // path[3] = path[0] + Vector3f(sinf(radians(18.0f)), cosf(radians(18.0f)), 0) * radius_cm;
-    // path[4] = path[0] + Vector3f(sinf(radians(18.0f)), -cosf(radians(18.0f)), 0) * radius_cm;
-    // path[5] = path[0] + Vector3f(-cosf(radians(36.0f)), sinf(radians(36.0f)), 0) * radius_cm;
-    // path[6] = path[1];   
-
-    //yz轴绘制五角星
-    path[1] = path[0] + Vector3f(0, 0, 1.0f) * radius_cm;
-    path[2] = path[0] + Vector3f(0, -sinf(radians(36.0f)), -cosf(radians(36.0f))) * radius_cm;
-    path[3] = path[0] + Vector3f(0, cosf(radians(18.0f)), sinf(radians(18.0f))) * radius_cm;
-    path[4] = path[0] + Vector3f(0, -cosf(radians(18.0f)), sinf(radians(18.0f))) * radius_cm;
-    path[5] = path[0] + Vector3f(0, sinf(radians(36.0f)), -cosf(radians(36.0f))) * radius_cm;
-    path[6] = path[1];   
+    path[0] = path[1] - Vector3f(1.0f, 0 ,0) * radius_cm;
+    path[2] = path[0] + Vector3f(-cosf(radians(36.0f)), -sinf(radians(36.0f)), 0) * radius_cm;
+    path[3] = path[0] + Vector3f(sinf(radians(18.0f)), cosf(radians(18.0f)), 0) * radius_cm;
+    path[4] = path[0] + Vector3f(sinf(radians(18.0f)), -cosf(radians(18.0f)), 0) * radius_cm;
+    path[5] = path[0] + Vector3f(-cosf(radians(36.0f)), sinf(radians(36.0f)), 0) * radius_cm;   
+    path[6] = path[1];
 
 }
 
@@ -50,7 +42,9 @@ void ModeDrawStar::pos_control_start()
     wp_nav->wp_and_spline_init();
 
     // no need to check return status because terrain data is not used
-    wp_nav->set_wp_destination(path[0], false);
+    // 这两个目的地的初始化也不知道 有啥区别，看着效果差不多
+    //wp_nav->set_wp_destination(path[1], false);
+    wp_nav->set_spline_destination(path[1], false, path[2], false, false);
 
     // initialise yaw
     auto_yaw.set_mode_to_default(false);
@@ -59,12 +53,16 @@ void ModeDrawStar::pos_control_start()
 //此模式的周期调用
 void ModeDrawStar::run()
 {
-    if (path_num < 6) {  // 五角星航线尚未走完
+    if (path_num < 5) {  // 五角星航线尚未走完
         if (wp_nav->reached_wp_destination()) {  // 到达某个端点
             path_num++;
-            wp_nav->set_wp_destination(path[path_num], false);  // 将下一个航点位置设置为导航控制模块的目标位置
+            wp_nav->set_spline_destination(path[path_num], false, path[path_num+1], false, true);  // 将下一个航点位置设置为导航控制模块的目标位置
         }
-    } else if ((path_num == 6) && wp_nav->reached_wp_destination()) {  // 五角星航线运行完成，自动进入Loiter模式
+    }else if ((path_num == 5) && wp_nav->reached_wp_destination()){
+        path_num = 6;
+        wp_nav->set_spline_destination(path[6], false, path[2], false, true);
+    }
+    else if ((path_num == 6) && wp_nav->reached_wp_destination()) {  // 五角星航线运行完成，自动进入Loiter模式
         gcs().send_text(MAV_SEVERITY_INFO, "Draw star finished, now go into loiter mode");
         copter.set_mode(Mode::Number::LOITER, ModeReason::MISSION_END);  // 切换到loiter模式
     }
